0efe4df971b660c927352597b75555a9b082f2a2
NahomAk <nahom.saketa108791@marwadiuniversity.ac.in>
2022-08-11 16:04:11 +0530

    up_app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
 import Signup from './Pages/Signup'
+import Login from './Pages/Login'
 import Components from './Page/Components';
 import './Assets/Styles/componentsPage.css'
 import './index.css'
@@ -19,6 +20,7 @@ function App() {
     <Router>
       <Routes>
          <Route  path='/signup' element={<Signup/>} />
+         <Route  path='/login' element={<Login />} />
           <Route  path='/' element={<Home/>} />
           <Route  path='/components' element={<Components/>} />
       </Routes>
