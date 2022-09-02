490539efb55a526c65675a6f6e5a1f10af370781
Engidawork <65005850+Engida2312@users.noreply.github.com>
2022-09-02 09:54:22 +0530

    dashboard

--- src/Layers/Side bar/Admin-sidebar.js
+++ src/Layers/Side bar/Admin-sidebar.js
@@ -1,7 +1,15 @@
-import '../../Assets/Styles/admin-sidebar.css'
 import SearchBar from "../../Componets/search-bar";
-
 import Logo from '../../Assets/Images/avatar.png'
+import { Link } from "react-router-dom";
+
+import {GoDashboard} from 'react-icons/go'
+import {CgComponents} from 'react-icons/cg'
+import {MdOutlineCategory} from 'react-icons/md'
+import {FiUsers} from 'react-icons/fi'
+
+import '../../Assets/Styles/admin-sidebar.css'
+import '../../Assets/Styles/accordion.css'
+
 const AdminSidebar = ()=>{
   return <>
 
@@ -17,10 +25,30 @@ const AdminSidebar = ()=>{
             </div>
         </div>
         <div className="a_sidebar_bottom center_center col">
-            <ul className='items_container'>
-                <li className='item'>Dahsboard</li>
-                <li className='item'>Dahsboard</li>
-                <li className='item'>Dahsboard</li>
+            <ul className="">
+                <li className="accordion_header"><Link to="/dashboard" className="link"><GoDashboard /> Dashbord</Link></li>
+                <li className="accordion_header link"><CgComponents/> Components</li>
+                <ul className="accordion_body_container">
+                    <div className="accordion_body">
+                        <li><a href="/all-events">All Components</a></li>
+                        <li><a href="/event/all/active">New Components</a></li>
+                        <li><a href="/add-event">Add Components</a></li>
+                    </div>
+                </ul>
+                <li className="accordion_header link"><MdOutlineCategory/> Catagoriies </li>
+                <ul className="accordion_body_container">
+                    <div className="accordion_body">
+                        <li><a href="/all-inactive-organizers">All Catagories</a></li>
+                        <li><a href="/organizer/register">Add catagory</a></li>
+                    </div>
+                </ul>
+                <li className="accordion_header link"><FiUsers/> Users </li>
+                <ul className="accordion_body_container">
+                    <div className="accordion_body">
+                        <li><a href="/all-inactive-organizers">Contributers</a></li>
+                        <li><a href="/organizer/register">All users</a></li>
+                    </div>
+                </ul>
             </ul>
         </div>
     </div>
