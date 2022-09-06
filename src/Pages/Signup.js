3d30385b4e444719087325930334049d391b1670
NahomAk <nahom.saketa108791@marwadiuniversity.ac.in>
2022-09-06 17:35:04 +0530

    validation

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import validation from "./validation";
+import { useState, useEffect } from "react";
 
 
 function Signup(){
+    const [values, setValues] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+    const [errors, setErrors] = useState({});
+    
+    const handleChange=(event)=>{
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value,
+        })
+    
+    }
+    const handleFormSubmit=(event)=>{
+        event.preventDefault();
+        setErrors(validation(values));
+    }
+
   return(
   <div>
     <body id="signup_body">
@@ -16,15 +37,21 @@ function Signup(){
                 <h1 id="signup_h1">sign up to EYN</h1>
                 <p className="signup_title">Discover The World's Top Creatives</p>
                 <div className="signup_inside">
-                    <label className="signup_label">Name	</label><input className="signup_input" type="Name" />
-                    <label className="signup_label">Email</label><input className="signup_input" type="email"  />
-                    <label className="signup_label">Password</label><input className="signup_input" type="password"  />
+
+                    <label className="signup_label">Name	</label><input className="signup_input" type="Name" id="name" name="name" value={values.name} onChange={handleChange} />
+                    {errors.name && <p className="error">{errors.name}</p>}
+
+                    <label className="signup_label">Email</label><input className="signup_input" type="email" id="email" name="email" value={values.email} onChange={handleChange}  />
+                    {errors.email && <p className="error">{errors.email}</p>}
+
+                    <label className="signup_label">Password</label><input className="signup_input" type="password" id="password" name="password" value={values.password}  onChange={handleChange} />
+                    {errors.password && <p className="error">{errors.password}</p>}
                     
                     <div class="signup_check">
                         <input id="signup_checkbox" type="checkbox" required/>
                         <label id="signup_boxagree" for="checkbox"> I agree to these <a className="termandcondition" href="#">Terms and Conditions</a>.</label>
                     </div>
-                    <button id="signup_button">CREATE ACCOUNT</button>
+                    <button id="signup_button" onClick={handleFormSubmit}>CREATE ACCOUNT</button>
                 </div>
 		    </form>
         </div>
---
+++ src/Pages/validation.js
@@ -0,0 +1,24 @@
+
+const validation = (values) => {
+
+    let errors={}
+
+    if(!values.name){
+        errors.name="Name is required"
+    }
+    if(!values.email){
+        errors.email="Email is required"
+    }else if(!/\S+@\S+\.\S+/.test(values.email)){
+        errors.email="The entered Email is invalid"
+    }
+    if(!values.password){
+        errors.password="Password is required"
+    }else if (values.password.length<8){
+        errors.password="Password must be more than 8"
+
+    }
+    return errors;
+
+}
+
+export default validation;
