2b0deeb2b2fec3afce6ce1e3b1e66b36cc720e59
NahomAk <nahom.saketa108791@marwadiuniversity.ac.in>
2022-09-07 11:16:23 +0530

    password more than 12

--- src/Pages/validation.js
+++ src/Pages/validation.js
@@ -13,7 +13,10 @@ const validation = (values) => {
     }
     if(!values.password){
         errors.password="Password is required"
-    }else if (values.password.length<8){
+    }else if(values.password.length>12){
+        errors.password="Password must not be more than 12"
+    }
+    else if (values.password.length<8){
         errors.password="Password must be more than 8"
 
     }
