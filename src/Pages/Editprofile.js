d2663c9537b0f2d6f1bc9ab28e13acffd93831b3
NahomAk <nahom.saketa108791@marwadiuniversity.ac.in>
2022-09-08 23:33:30 +0530

    edit_profile_page

--- src/Pages/Editprofile.js
+++ src/Pages/Editprofile.js
@@ -1,11 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import {GoDashboard} from 'react-icons/go'
-import {CgComponents} from 'react-icons/cg'
-import {MdOutlineCategory} from 'react-icons/md'
-import {FiUsers} from 'react-icons/fi'
-
 import './../Assets/Styles/editprofile.css'
 import Logo from './../Assets/Images/avatar.png'
 
@@ -15,46 +9,33 @@ import Logo from './../Assets/Images/avatar.png'
 function Editprofile(){
     return(<div>
        
-       
-    <div className="a_sidebar_container">
-        
-        <div className="a_sidebar_bottom center_center col">
-            <ul className="">
-            <li className="accordion_header link"><Link to="/editprofile" className="link"> Edit Profile</Link> </li>
-                <li className="accordion_header"><Link to="" className="link"> Home</Link></li>
-                <li className="accordion_header link"><Link to="/components" className="link"> Components</Link></li>
-                
-                <li className="accordion_header link"><Link to="/profile" className="link">Profile </Link></li>
-                
-               
-                
-            </ul>
-        </div>
-    </div>
-    
-
     <div className="right">
     <h2 className="edit_h2">Edit Profile</h2>
     
         
 	   		<img className="edit_img" src={Logo} alt="av"/>
-            <input type="file" name="file" className="imgpic" />
+            <input type="file" name="file" className="imgpic input_field_img" />
 
 		
-        <form className="edit_form">
-            
-        <fieldset className="edit_fieldset">
-        <label >First Name: <input type="text" name="first-name" className="input_field" /></label>
-        <label>Last Name: <input type="text" name="last-name" className="input_field" /></label>
-        <label> Email: <input type="email" name="email" className="input_field" /></label>
-        <label> Linkedin Link: <input type="link" name="lin" className="input_field" /></label>
-        <label> Github Link: <input type="link" name="git" className="input_field" /></label>
-        
-      </fieldset>
-      <input type="submit" value="Save" className="btn save"/>
-      
-      
-        </form>
+
+
+        <form>
+                <ul class="form-style-1">
+                    <li><label>Full Name </label><input type="text" name="field1" class="field-divided" placeholder="First" /> <input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
+                    <li>
+                        <label>Email </label>
+                        <input type="email" name="field3" class="field-long" placeholder="nahom@mail.com"/>
+                        <label>Linkedin Link </label>
+                        <input type="email" name="field3" class="field-long" placeholder="linkedin.com/in/nahom-saketa-64970a245/"/>
+                        <label>Github Link </label>
+                        <input type="email" name="field3" class="field-long"placeholder="https://github.com/Na-ak-11" />
+                        <label>Role </label>
+                        <input type="email" name="field3" class="field-long" placeholder="Developer"/>
+                        <li><label>Change Password</label><input type="text" name="field1" class="field-divided" placeholder="New Password" /> <input type="text" name="field2" class="field-divided" placeholder="Confirm Password" /></li>
+                    </li>
+                </ul>
+                <input type="submit" value="save" className="btn save"/>
+         </form>
 
 
 
