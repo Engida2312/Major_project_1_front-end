8796a91816a59a6ba99501f8e1ad3b5aca3ddaea
Engidawork <65005850+Engida2312@users.noreply.github.com>
2022-10-06 02:19:16 -0700

    index change

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './Redux/store'
-import { AuthProvider } from './Context/AuthProvider';
+// import { AuthProvider } from './Context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <AuthProvider>
-      <App />
-    </AuthProvider> */}
     <Provider store={store}>
       <App  />
     </Provider>
